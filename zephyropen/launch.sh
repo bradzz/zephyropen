2316e37848c91bb8bacceb507bbcd9be8872a6f4
brad.zdanivsky@gmail.com <brad.zdanivsky@gmail.com@e44650f9-a4ec-bc1f-af25-49e015dfa90f>
2011-01-10 03:04:54 +0000

    osx, linux launch example

---
+++ zephyropen/launch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+java -classpath ./bin:./lib/bluecove.jar:./lib/charts4j.jar:./lib/BrowserLauncher.jar zephyropen.swing.gui.BluetoothGUI &
+
+
+# java -classpath "./bin:./lib/bluecove.jar:./lib/charts4j.jar:./lib/BrowserLauncher.jar" zephyropen.swing.gui.viewer.DeviceViewer brad &
