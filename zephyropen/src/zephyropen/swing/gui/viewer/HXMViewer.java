81a99076b810159215051a8db12366fb4a4693ba
brad.zdanivsky@gmail.com <brad.zdanivsky@gmail.com@e44650f9-a4ec-bc1f-af25-49e015dfa90f>
2011-04-08 01:39:48 +0000

    hxm

--- zephyropen/src/zephyropen/swing/gui/viewer/HXMViewer.java
+++ zephyropen/src/zephyropen/swing/gui/viewer/HXMViewer.java
@@ -11,27 +11,66 @@ import zephyropen.util.google.GoogleLineGraph;
 
 public class HXMViewer extends AbstractViewer implements Viewer {
 
-    public HXMViewer(API caller) {
+	private int seq, last = 0;
 
-        api = caller;
+	public HXMViewer(API caller) {
 
-        charts = new GoogleChart[5];
-        charts[0] = new GoogleLineGraph(PrototypeFactory.heart, "bpm", Color.RED);
-        charts[1] = new GoogleLineGraph(PrototypeFactory.speed, "m/s", Color.ORANGE);
-        charts[2] = new GoogleLineGraph(PrototypeFactory.distance, "meters", Color.BROWN);
-        charts[3] = new GoogleLineGraph(PrototypeFactory.beat, "seq", Color.ORANGERED);
-        charts[4] = new GoogleLineGraph(PrototypeFactory.connection, "sec", Color.FORESTGREEN);
+		api = caller;
 
-        frame = new TabbedFrame(charts);
-    }
+		charts = new GoogleChart[6];
+		charts[0] = new GoogleLineGraph(PrototypeFactory.heart, "bpm", Color.RED);
+		charts[1] = new GoogleLineGraph(PrototypeFactory.rr, "ms", Color.DARKBLUE);
+		charts[2] = new GoogleLineGraph(PrototypeFactory.speed, "m/s", Color.ORANGE);
+		charts[3] = new GoogleLineGraph(PrototypeFactory.distance, "meters", Color.BROWN);
+		charts[4] = new GoogleLineGraph(PrototypeFactory.beat, "seq", Color.ORANGERED);
+		charts[5] = new GoogleLineGraph(PrototypeFactory.connection, "sec", Color.FORESTGREEN);
 
-    /** */
-    public void update(Command command) {
-        battery = command.get(PrototypeFactory.battery);
-        charts[0].add(command.get(PrototypeFactory.heart));
-        charts[1].add(command.get(PrototypeFactory.speed));
-        charts[2].add(command.get(PrototypeFactory.distance));
-        charts[3].add(command.get(PrototypeFactory.beat));
-        charts[4].add(String.valueOf(api.getDelta()));
-    }
+
+		frame = new TabbedFrame(charts);
+	}
+
+	/** */
+	public void update(Command command) {
+
+		String beat = command.get(PrototypeFactory.heart);
+		seq = Integer.parseInt(beat);
+
+		if (!(seq == (last - 1))) {
+
+			constants.info("seq: " + seq + " last: " + last, this);
+		
+		} else if( seq == last ){
+			
+			constants.error("same beat?", this);
+			return;
+			
+		}
+
+		
+		//
+/*
+		for (int i = 0; i < 4; i++) {
+			String value = command.get(PrototypeFactory.rr + i);
+			System.out.println("[" + i + "]: " + value);
+		}
+*/
+		
+		int v1 = Integer.parseInt(command.get("rr1"));
+		int v2 = Integer.parseInt(command.get("rr2"));
+        int result =  Math.abs(v1 - v2);
+        
+		if(result < 300  ) result = 300;
+		if(result > 1600 ) result = 1600;
+		
+		System.out.println("res: " + result);
+		
+		charts[0].add(command.get(PrototypeFactory.heart));
+		charts[1].add(Integer.toString(result));
+		charts[2].add(command.get(PrototypeFactory.speed));
+		charts[3].add(command.get(PrototypeFactory.distance));
+		charts[4].add(command.get(PrototypeFactory.beat));
+		charts[5].add(String.valueOf(api.getDelta()));
+		battery = command.get(PrototypeFactory.battery);
+		last = seq;
+	}
 }
